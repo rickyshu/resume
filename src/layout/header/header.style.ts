4f297dff0ecaf5190df7632fdb8cdec9a131873f
rickyshu <hoinleekk@naver.com>
2023-06-01 13:15:17 +0900

    design: header 레이아웃 구현

--- src/layout/header/header.style.ts
+++ src/layout/header/header.style.ts
@@ -1,11 +1,77 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { RiSunFill } from "react-icons/ri";
+import { RiMoonClearFill } from "react-icons/ri";
 
-export const Header = styled.header`
+interface HedearProps {
+  headerColor: boolean;
+}
+export const Header = styled.header<HedearProps>`
   position: fixed;
   top: 0;
-  border: 1px solid blue;
   width: 100vw;
-  height: 65px;
+  height: 80px;
   z-index: 1;
-  background-color: blue;
+  background-color: ${(props) => (props.headerColor ? "#e41c6f" : "transparent")};
+  color: hsla(0, 0%, 100%, 0.7);
+  & .active {
+    background-color: #e41c6f;
+  }
+`;
+
+export const Wrapper = styled.section`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1.5rem;
+  border: 1px solid red;
+`;
+
+export const Logo = styled.div`
+  font-size: 1.5rem;
+  font-weight: 700;
+`;
+
+export const LeftWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const Link = styled.a`
+  padding: 0.5rem 0.5rem;
+  cursor: pointer;
+  &:hover {
+    border-bottom: 3px solid black;
+  }
+`;
+
+export const ThemeWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 0.8rem;
+`;
+
+const CommonStyle = css`
+  font-size: 1.5rem;
+  cursor: pointer;
+`;
+
+export const SunIcon = styled(RiSunFill)`
+  ${CommonStyle};
+`;
+
+export const MoonIcon = styled(RiMoonClearFill)`
+  ${CommonStyle};
 `;
--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,7 +1,54 @@
 import * as S from "./header.style";
 
+import { useState } from "react";
+
+import { themeMode } from "../../recoil/Theme";
+import { useRecoilState } from "recoil";
+
+type LinkName = readonly ["About Me", "Introduction", "Skills", "Blogs", "Projects", "Studies"];
+
 const Header = () => {
-  return <S.Header>header</S.Header>;
+  const navList: LinkName = ["About Me", "Introduction", "Skills", "Blogs", "Projects", "Studies"];
+
+  const [headerColor, setHeaderColor] = useState<boolean>(false);
+
+  const [handleThemeMode, setHandleThemeMode] = useRecoilState<string>(themeMode);
+
+  const handleTheme = () => {
+    if (handleThemeMode === "dark") {
+      localStorage.setItem("theme", "light");
+      setHandleThemeMode("light");
+    } else if (handleThemeMode === "light") {
+      localStorage.setItem("theme", "dark");
+      setHandleThemeMode("dark");
+    }
+  };
+
+  const changeHeaderColor = () => {
+    if (window.scrollY >= 30) {
+      setHeaderColor(true);
+    } else {
+      setHeaderColor(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeHeaderColor);
+
+  return (
+    <S.Header headerColor={headerColor}>
+      <S.Wrapper>
+        <S.Logo>Rickyshu's Portfolio</S.Logo>
+        <S.LeftWrapper>
+          <S.Nav>
+            {navList.map((element, index) => {
+              return <S.Link key={index}>{element}</S.Link>;
+            })}
+          </S.Nav>
+          <S.ThemeWrapper onClick={handleTheme}>{handleThemeMode === "light" ? <S.SunIcon /> : <S.MoonIcon />}</S.ThemeWrapper>
+        </S.LeftWrapper>
+      </S.Wrapper>
+    </S.Header>
+  );
 };
 
 export default Header;
