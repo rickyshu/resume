5c2ce4a0fe4930e9de86e2304a81afdf38a5e261
rickyshu <hoinleekk@naver.com>
2023-06-08 21:36:52 +0900

    design:footer 레이아웃 구현

--- src/layout/footer/Footer.style.ts
+++ src/layout/footer/Footer.style.ts
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 
 export const Footer = styled.section`
-  width: 100%;
-  height: 50px;
-  background-color: black;
-  /* position: fixed;
-  bottom: 0; */
-  border: 1px solid green;
+  padding: 1rem;
+  background-color: #e41c6f;
+  color: hsla(0, 0%, 100%, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
+
+export const Reserved = styled.div``;
+
+export const UpdateInfo = styled.div``;
--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import * as S from "./Footer.style";
 
 const Footer = () => {
-  return <S.Footer>Footer</S.Footer>;
+  return (
+    <S.Footer>
+      <S.Reserved>© 2023. LEE HO IN. All rights reserved.</S.Reserved>
+      <S.UpdateInfo>Latest update 2023.06</S.UpdateInfo>
+    </S.Footer>
+  );
 };
 
 export default Footer;
