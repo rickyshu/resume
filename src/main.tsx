966c2b6695febf1622645b24700f61f06914a4b5
rickyshu <hoinleekk@naver.com>
2023-06-15 14:55:23 +0900

    chore:react query 적용

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,19 @@ import App from "./App.tsx";
 // recoil
 import { RecoilRoot } from "recoil";
 
+// react query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({ defaultOptions: { queries: { staleTime: 1000 * 25 } } });
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>
 );
